3b7545a6c35816d1c737a5a17209c812b71fb3a0
Jeremy Adams <jeremy@puppetlabs.com>
2014-04-29 16:05:20 -0700

    more

--- files/rolltool.rb
+++ files/rolltool.rb
@@ -6,7 +6,9 @@ module MCollective
         login_cmd = "accurev login #{request[:login]} #{request[:password]}"
         clone_cmd = "accurev mkws -w #{request[:wsname]} -b #{request[:stream]} -l #{request[:path]}"
         logout_cmd = "accurev logout"
-
+        `login_cmd`
+        `clone_cmd`
+        `logout_cmd`
       end
 
     end
