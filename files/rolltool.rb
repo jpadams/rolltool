8628f63ff52d8c10ca47ee34c4a214c83fa2d349
Jeremy Adams <jeremy@puppetlabs.com>
2014-04-30 06:40:59 -0700

    basically working. need to test on system with accurev

--- files/rolltool.rb
+++ files/rolltool.rb
@@ -3,9 +3,12 @@ module MCollective
     class Service<RPC::Agent
 
       action 'roll' do
-        login_cmd = "accurev login #{request[:login]} #{request[:password]}"
-        clone_cmd = "accurev mkws -w #{request[:wsname]} -b #{request[:stream]} -l #{request[:path]}"
-        logout_cmd = "accurev logout"
+        #login_cmd = "accurev login #{request[:login]} #{request[:password]}"
+        login_cmd = 'echo "login complete" | cat > /tmp/foo'
+        #clone_cmd = "accurev mkws -w #{request[:wsname]} -b #{request[:stream]} -l #{request[:path]}"
+        clone_cmd = 'echo "clone complete" | cat >> /tmp/foo' 
+        #logout_cmd = "accurev logout"
+        logout_cmd = 'echo "logout complete" | cat >> /tmp/foo' 
         `login_cmd`
         `clone_cmd`
         `logout_cmd`
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,1 +1,21 @@
-class rolltool {}
+class rolltool {
+
+  file { "/opt/puppet/libexec/mcollective/mcollective/agent/rolltool.ddl":
+      ensure  => $ensure,
+      owner   => root,
+      group   => root,
+      mode    => '0644',
+      source  => 'puppet://modules/rolltool/rolltool.ddl',
+      notify  => Service['pe-mcollective'],
+    } ->
+
+  file { "/opt/puppet/libexec/mcollective/mcollective/agent/rolltool.rb":
+      ensure  => $ensure,
+      owner   => root,
+      group   => root,
+      mode    => '0644',
+      source  => 'puppet://modules/rolltool/rolltool.rb',
+      notify  => Service['pe-mcollective'],
+    }
+
+}
